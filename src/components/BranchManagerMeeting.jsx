ee77bb4c33bad87ac03e026263fb0935d839f9a2
Tyler <tymurrayco@gmail.com>
2025-05-27 08:24:03 -0700

    Making border easier to see

--- src/components/BranchManagerMeeting.jsx
+++ src/components/BranchManagerMeeting.jsx
@@ -1364,7 +1364,7 @@ const BranchManagerMeeting = () => {
                                         }
                                       }}
                                       placeholder="Enter actions & deadlines..."
-                                      className="w-full px-3 py-2 bg-white border border-black hover:bg-gray-50 focus:bg-white focus:border focus:rounded-md focus:outline-none resize-none"
+                                      className="w-full px-3 py-2 bg-white border-2 border-black hover:bg-gray-50 focus:bg-white focus:border focus:rounded-md focus:outline-none resize-none"
                                       style={{
                                         height: '5rem',
                                         overflowY: 'auto'
